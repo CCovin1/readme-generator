391cbd079e9174722d7acb5ba798ad2df8856f90
CCovin1 <caleb.covin@gmail.com>
2023-07-20 10:26:47 -0500

    added the function with the componets to generate teh readme

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -16,3 +16,77 @@ function renderLicenseBadge(license) {
   // If no license or invalid license is provided, return an empty string
   return '';
 }
+// Function to get the license link based on the license passed in
+function renderLicenseLink(license) {
+  if (license) {
+    // Assuming the license URLs, replace with actual URLs if available
+    const licenseLinks = {
+      'MIT': 'https://opensource.org/licenses/MIT',
+    };
+
+    // Check if the provided license exists in the licenseLinks object
+    if (licenseLinks[license]) {
+      return licenseLinks[license];
+    }
+  }
+
+  // If no license or invalid license is provided, return an empty string
+  return '';
+}
+
+// Function to get the license section of README based on the license passed in
+function renderLicenseSection(license) {
+  if (license) {
+    return `## License
+
+This project is licensed under the [${license}](${renderLicenseLink(license)}) license. See the [LICENSE](./LICENSE) file for details.`;
+  }
+
+  // If no license or invalid license is provided, return an empty string
+  return '';
+}
+
+// Function to generate markdown for README
+function generateMarkdown(data) {
+  return `# ${data.title}
+
+${renderLicenseBadge(data.license)}
+
+## Description
+
+${data.description}
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [License](#license)
+- [Contact](#contact)
+
+## Installation
+
+${data.installation}
+
+## Usage
+
+${data.usage}
+
+## Contributing
+
+${data.contributing}
+
+## Tests
+
+${data.tests}
+
+${renderLicenseSection(data.license)}
+
+## Contact
+
+If you have any questions or concerns about this project, you can reach me at ${data.email}.`;
+
+}
+
+module.exports = generateMarkdown;
