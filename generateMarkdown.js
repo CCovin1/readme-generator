9f3a5f0ba6e78a0e48ae5ddcda36018aab771b06
CCovin1 <caleb.covin@gmail.com>
2023-07-20 10:23:21 -0500

    added a function to get the license badges

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -1,20 +1,9 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+// Function to get the license badge based on the license passed in
+function renderLicenseBadge(license) {
+  if (license) {
+    // Assuming the license badge image URLs, replace with actual image URLs if available
+    const badges = {
+      'MIT': '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)',
+      // Add more license badges here as needed
+    };
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
-
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
-
-// TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
-
-`;
-}
-
-module.exports = generateMarkdown;
